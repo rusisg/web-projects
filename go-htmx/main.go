8bfff6c328566de504ccf6b2b3d0219114c35b09
rusisg <rasul.nulrybekov@gmail.com>
2024-12-12 00:58:57 +0100

    start

--- go-htmx/main.go
+++ go-htmx/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"go-htmx/handler"
+	"log"
 	"net/http"
-	"time"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
-}
-
 func main() {
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler.Greet)
+	mux.HandleFunc("/blog", handler.Blog)
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
 }
